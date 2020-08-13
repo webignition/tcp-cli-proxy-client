cc59c138ff8d38f06c335c50e323b832e1182bc0
Jon Cram <webignition@gmail.com>
2020-08-13 18:41:18 +0100

    poc client writes to resource

--- tests/Integration/client.php
+++ tests/Integration/client.php
@@ -1,13 +1,15 @@
 <?php
 
 $fp = stream_socket_client("tcp://localhost:8000", $errno, $errstr, 30);
+$out = STDOUT;
 
 if (!$fp) {
     echo "$errstr ($errno)<br />\n";
 } else {
     fwrite($fp, "./tests/Integration/fixture.sh\n");
     while (!feof($fp)) {
-        echo fgets($fp);
+        $buffer = fgets($fp);
+        fwrite($out, $buffer);
     }
     fclose($fp);
 }
