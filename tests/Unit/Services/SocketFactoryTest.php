53cfdf6a8d385201c4afc79b6714c67b2ef2313e
Jon Cram <webignition@gmail.com>
2020-08-19 10:49:52 +0100

    Add ClientCreationException::connectionString

--- tests/Unit/Services/SocketFactoryTest.php
+++ tests/Unit/Services/SocketFactoryTest.php
@@ -7,6 +7,8 @@ namespace webignition\TcpCliProxyClient\Tests\Unit\Services;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use phpmock\mockery\PHPMockery;
 use PHPUnit\Framework\TestCase;
+use webignition\ObjectReflector\ObjectReflector;
+use webignition\TcpCliProxyClient\Exception\ClientCreationException;
 use webignition\TcpCliProxyClient\Services\ErrorHandler;
 use webignition\TcpCliProxyClient\Services\SocketFactory;
 
@@ -14,14 +16,83 @@ class SocketFactoryTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
 
-    public function testCreate()
+    private string $connectionString;
+    private SocketFactory $factory;
+
+    protected function setUp(): void
     {
+        parent::setUp();
+
         $host = 'localhost';
         $port = 8000;
+        $this->connectionString = (string) sprintf('tcp://%s:%d', $host, $port);
 
+        $this->factory = new SocketFactory($this->createErrorHandler());
+    }
+
+    public function testCreateSuccess()
+    {
         /** @var resource $socket */
         $socket = \Mockery::mock();
 
+        PHPMockery::mock('webignition\TcpCliProxyClient\Services', 'is_resource')
+            ->with($socket)
+            ->andReturnTrue();
+
+        PHPMockery::mock('webignition\TcpCliProxyClient\Services', 'stream_socket_client')
+            ->withArgs(function (string $connectionString, $errorNumber, $errorMessage) {
+                $this->assertStreamSocketClientArguments($connectionString, $errorNumber, $errorMessage);
+
+                return true;
+            })
+            ->andReturn($socket);
+
+        $createdSocket = $this->factory->create($this->connectionString);
+        self::assertSame($socket, $createdSocket);
+    }
+
+    public function testCreateThrowsClientCreationException()
+    {
+        $socketErrorMessage = 'socket error message';
+        $socketErrorNumber = 123;
+
+        PHPMockery::mock('webignition\TcpCliProxyClient\Services', 'stream_socket_client')
+            ->withArgs(function (
+                string $connectionString,
+                $errorNumber,
+                $errorMessage
+            ) use (
+                $socketErrorMessage,
+                $socketErrorNumber
+            ) {
+                $this->assertStreamSocketClientArguments($connectionString, $errorNumber, $errorMessage);
+
+                ObjectReflector::setProperty($this->factory, SocketFactory::class, 'errorNumber', $socketErrorNumber);
+                ObjectReflector::setProperty($this->factory, SocketFactory::class, 'errorMessage', $socketErrorMessage);
+
+                return true;
+            })
+            ->andReturn(false);
+
+        try {
+            $this->factory->create($this->connectionString);
+            self::fail('ClientCreationException not thrown');
+        } catch (ClientCreationException $clientCreationException) {
+            self::assertEquals(
+                new ClientCreationException(
+                    $this->connectionString,
+                    $socketErrorMessage,
+                    $socketErrorNumber
+                ),
+                $clientCreationException
+            );
+
+            self::assertSame($this->connectionString, $clientCreationException->getConnectionString());
+        }
+    }
+
+    private function createErrorHandler(): ErrorHandler
+    {
         $errorHandler = \Mockery::mock(ErrorHandler::class);
         $errorHandler
             ->shouldReceive('start')
@@ -31,23 +102,18 @@ class SocketFactoryTest extends TestCase
             ->shouldReceive('stop')
             ->once();
 
-        PHPMockery::mock('webignition\TcpCliProxyClient\Services', 'is_resource')
-            ->with($socket)
-            ->andReturnTrue();
+        return $errorHandler;
+    }
 
-        PHPMockery::mock('webignition\TcpCliProxyClient\Services', 'stream_socket_client')
-            ->withArgs(function (string $connectionString, $errorNumber, $errorMessage) use ($host, $port) {
-                self::assertSame('tcp://' . $host . ':' . $port, $connectionString);
-                self::assertNull($errorNumber);
-                self::assertNull($errorMessage);
-
-                return true;
-            })
-            ->andReturn($socket);
-
-        $factory = new SocketFactory($errorHandler);
-
-        $createdSocket = $factory->create($host, $port);
-        self::assertSame($socket, $createdSocket);
+    /**
+     * @param string $connectionString
+     * @param null $errorNumber
+     * @param null $errorMessage
+     */
+    private function assertStreamSocketClientArguments(string $connectionString, $errorNumber, $errorMessage): void
+    {
+        self::assertSame($this->connectionString, $connectionString);
+        self::assertNull($errorNumber);
+        self::assertNull($errorMessage);
     }
 }
