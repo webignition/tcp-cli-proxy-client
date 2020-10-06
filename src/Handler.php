6d56b195cf927031cdd187aa04f3497dea4dc495
Jon Cram <webignition@gmail.com>
2020-10-06 15:06:43 +0100

    Return content via handler

---
+++ src/Handler.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\TcpCliProxyClient;
+
+class Handler
+{
+    /**
+     * @var resource
+     */
+    private $socket;
+
+    /**
+     * @var callable[]
+     */
+    private array $callbacks = [];
+
+    public function handle(): void
+    {
+        while (!feof($this->socket)) {
+            $buffer = (string) fgets($this->socket);
+
+            foreach ($this->callbacks as $callback) {
+                $callbackReturn = $callback($buffer);
+
+                if (null !== $callbackReturn) {
+                    $buffer = $callbackReturn;
+                }
+            }
+        }
+    }
+
+    public function addCallback(callable $callback): self
+    {
+        $this->callbacks[] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * @param resource $socket
+     *
+     * @return $this
+     */
+    public function withSocket($socket): self
+    {
+        $this->socket = $socket;
+
+        return $this;
+    }
+}
