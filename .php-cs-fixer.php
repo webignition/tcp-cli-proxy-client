e97b2b9230acea7bfb192fca72dc16e7abeec647
Jon Cram <webignition@gmail.com>
2026-02-06 16:01:31 +0000

    Update workflow actions (#60)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -14,4 +14,9 @@ return $config->setRules([
     'trailing_comma_in_multiline' => false,
     'php_unit_internal_class' => false,
     'php_unit_test_class_requires_covers' => false,
+    // Following lines added to fix #55
+    // @todo remove in #61
+    'phpdoc_order' => false,
+    'phpdoc_separation' => false,
+    'phpdoc_trim' => false,
 ])->setFinder($finder);
