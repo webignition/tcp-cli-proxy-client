8eaeef6bd71b4e74accfa7bc468d388f8c90fdf6
Jon Cram <webignition@gmail.com>
2026-02-06 16:14:56 +0000

    Swap PSR-12 for PER-CS

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,7 +6,7 @@ $finder = PhpCsFixer\Finder::create()
 
 $config = new PhpCsFixer\Config();
 return $config->setRules([
-    '@PSR12' => true,
+    '@PER-CS' => true,
     '@PhpCsFixer' => true,
     'concat_space' => [
         'spacing' => 'one',
@@ -14,9 +14,4 @@ return $config->setRules([
     'trailing_comma_in_multiline' => false,
     'php_unit_internal_class' => false,
     'php_unit_test_class_requires_covers' => false,
-    // Following lines added to fix #55
-    // @todo remove in #61
-    'phpdoc_order' => false,
-    'phpdoc_separation' => false,
-    'phpdoc_trim' => false,
 ])->setFinder($finder);
